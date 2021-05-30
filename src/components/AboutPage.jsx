903ece6eb34857a0fcf38799d513a9f59e7c1de7
Oscar Caparros <ocaparrostortosa@gmail.com>
2021-05-30 16:31:56 +0200

    Modifying the home page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useLocation} from "react-router-dom";
 
-
+// Funcion de ejemplo para obtener la ruta en la que nos encontramos
+function usePageViews() {
+    let location = useLocation();
+    React.useEffect(() => {
+     console.log(location.pathname);
+    }, [location]);
+  }
 
 const AboutPage = () => {
 
-
+    usePageViews();
     return (
-
+        
         <div></div>
     )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,42 @@
+import { Avatar, Card, CardContent, CardHeader, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
+import { Fragment } from 'react'
+
 
 const HomePage = () => {
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          display: 'flex',
+          '& > *': {
+            margin: theme.spacing(1),
+          },
+        },
+        small: {
+          width: theme.spacing(3),
+          height: theme.spacing(3),
+        },
+        large: {
+          width: theme.spacing(10),
+          height: theme.spacing(10),
+        },
+      }));
+
+    const classes = useStyles();
+
     return (
-        <div>Home</div>
+        <Fragment>
+            <Card className={classes.cardStyles}>
+                <CardHeader
+                    avatar={<Avatar alt="React logo" src="../static/images/avatar/react2.png" />}
+                    title={<Typography className={classes.title}>React Apirest</Typography>}
+                    className={classes.cardHeader}
+                />
+                <CardContent>
+                    <Avatar alt="React logo"className={classes.large} src="../static/images/avatar/react.svg"/>
+                </CardContent>
+            </Card>
+        </Fragment>
     )
 }
 
